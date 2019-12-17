8dc5f2ba9fca2f806c220120ba2ee1c222bc13e3
krzysq23 <krzysztof.skarbowski@gmail.com>
2019-12-17 10:54:47 +0100

    Clean Project

---
+++ TestAppJenkins/src/main/java/com/spring/TestAppJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppJenkinsApplication.class, args);
+	}
+
+}
