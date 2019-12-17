bc031009ca1a5631a95cac60e3407d5075f5a9ad
krzysq23 <krzysztof.skarbowski@gmail.com>
2019-12-17 13:25:28 +0100

    Hello controller

---
+++ src/main/java/com/spring/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+    
+}
